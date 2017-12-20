1035e080737c272f7877f64051d06602e6ef2a69
tcdw <admin@tcdw.net>
2017-12-20 15:50:17 +0800

    大体框架完成

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+
+const imagemagick = require('imagemagick');
+const http = require('http');
+const fs = require('fs');
+
+if (typeof process.argv[2] === 'undefined') {
+    console.error('Usage: oneanime config_file');
+    process.exit(1);
+}
+
+const config = JSON.parse(fs.readFileSync(process.argv[2], { encoding: 'utf8' }));
+
+
+http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('test');
+    }
+}).listen(8080, 'localhost');
